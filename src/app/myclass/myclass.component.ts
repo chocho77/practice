a27b3e185863811932803909905271d2b91163ce
chavdar77 <chavmom@gmail.com>
2023-06-12 17:59:25 -0400

    Changes to be committed: 	modified:   src/app/myclass/myclass.component.html 	modified:   src/app/myclass/myclass.component.ts 	new file:   src/app/post/Person.ts

--- src/app/myclass/myclass.component.ts
+++ src/app/myclass/myclass.component.ts
@@ -1,13 +1,30 @@
 import { Component,Input } from '@angular/core';
+import { Person } from '../post/Person';
+import { Pingible } from '../Pingible';
 
 @Component({
   selector: 'app-myclass',
   templateUrl: './myclass.component.html',
   styleUrls: ['./myclass.component.css']
 })
-export class MyclassComponent {
+export class MyclassComponent implements Pingible{
+  firstName!:string;
+  lastName!:string;
+  emailadress!:string;
+
   @Input()
   fromParent!: string;
   childMessage:string = 'From Chiled Component';
+
+  constructor() {
+    this.ping();
+  }
+  ping(): void {
+    const person = new Person();
+    this.firstName = person.firstName;
+    this.lastName = person.lastName;
+    this.emailadress = person.emailPerson;
+    
+  }
 
 }
---
+++ src/app/post/Person.ts
@@ -0,0 +1,11 @@
+export class Person {
+    firstName!:string;
+    lastName!:string;
+    emailPerson!:string;
+
+    constructor() {
+        this.firstName = "Chavdar";
+        this.lastName = "Momchilov";
+        this.emailPerson = "chavmom@gmail.com";
+    }
+}
