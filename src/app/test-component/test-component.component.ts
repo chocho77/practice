0c3ee6117b03a7e6e2747ac526c770d6dd93d218
chavdar77 <chavmom@gmail.com>
2023-06-14 02:17:24 -0400

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   src/app/test-component/test-component.component.html 	modified:   src/app/test-component/test-component.component.ts

--- src/app/test-component/test-component.component.ts
+++ src/app/test-component/test-component.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
 export class TestComponentComponent {
 
   message:string = 'Message From Typescript Component File.';
+  imgUrl:string = 'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80';
 
+  
 }
